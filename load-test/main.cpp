3f76030d6a1f7937d2b0abb029211d92d3bbf5fb
Arne Claus <arne.claus@trivago.com>
2018-02-07 20:25:35 +0100

    Initial Commit

---
+++ load-test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  load-test
+//
+//  Created by Arne Claus on 07.02.18.
+//  Copyright © 2018 Arne Claus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
